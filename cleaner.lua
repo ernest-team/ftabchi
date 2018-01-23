9f0931973e4dfd3ec8f0d9b7c7e40e46dfe97e98
ErnestTeam <34784162+ernest-team@users.noreply.github.com>
2018-01-24 02:54:52 +0330

    Update cleaner.lua

--- cleaner.lua
+++ cleaner.lua
@@ -1,44 +1,22 @@
 redis = (loadfile "redis.lua")()
-
 function gettabchiid()
-
     local i, t, popen = 0, {}, io.popen
-
     local pfile = popen('ls')
-
 	local last = 0
-
     for filename in pfile:lines() do
-
-        if filename:match('tabchi%-(%d+)%.lua') and tonumber(filename:match('tabchi%-(%d+)%.lua')) >= last then
-
-			last = tonumber(filename:match('tabchi%-(%d+)%.lua')) + 1
-
+        if filename:match('ftabchi%-(%d+)%.lua') and tonumber(filename:match('ftabchi%-(%d+)%.lua')) >= last then
+			last = tonumber(filename:match('ftabchi%-(%d+)%.lua')) + 1
 			end		
-
     end
-
     return last
-
 end
-
 local last = gettabchiid()
-
-io.write("Auto Detected Tabchi ID : "..last)
-
+io.write("Auto Detected Ftabchi ID : "..last)
 io.write("\nEnter Full Sudo ID : ")
-
 local sudo=io.read()
-
-local text,ok = io.open("base.lua",'r'):read('*a'):gsub("TABCHI%-ID",last)
-
-io.open("tabchi-"..last..".lua",'w'):write(text):close()
-
-io.open("tabchi-"..last..".sh",'w'):write("while true; do\n$(dirname $0)/telegram-cli-1222 -p tabchi-"..last.." -s tabchi-"..last..".lua\ndone"):close()
-
+local text,ok = io.open("base.lua",'r'):read('*a'):gsub("FTABCHI%-ID",last)
+io.open("ftabchi-"..last..".lua",'w'):write(text):close()
+io.open("ftabchi-"..last..".sh",'w'):write("while true; do\n$(dirname $0)/telegram-cli-1222 -p ftabchi-"..last.." -s ftabchi-"..last..".lua\ndone"):close()
 io.popen("chmod 777 tabchi-"..last..".sh")
-
-redis:set('tabchi:'..last..':fullsudo',sudo)
-
-print("Done!\nNew Tabchi Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./tabchi-"..last..".sh\nDeveloped by: JoveTeam")
-
+redis:set('ftabchi:'..last..':fullsudo',sudo)
+print("Done!\nNew Ftabchi Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./ftabchi-"..last..".sh\nDeveloped by: ernestteam")
