be734acb96585e01c53df1a1acdcc3cf7a428440
ErnestTeam <34784162+ernest-team@users.noreply.github.com>
2018-01-24 03:13:32 +0330

    Update cleaner.lua

--- cleaner.lua
+++ cleaner.lua
@@ -1,22 +1,12 @@
 redis = (loadfile "redis.lua")()
-function gettabchiid()
-    local i, t, popen = 0, {}, io.popen
-    local pfile = popen('ls')
-	local last = 0
-    for filename in pfile:lines() do
-        if filename:match('ftabchi%-(%d+)%.lua') and tonumber(filename:match('ftabchi%-(%d+)%.lua')) >= last then
-			last = tonumber(filename:match('ftabchi%-(%d+)%.lua')) + 1
-			end		
-    end
-    return last
-end
-local last = gettabchiid()
-io.write("Auto Detected Ftabchi ID : "..last)
-io.write("\nEnter Full Sudo ID : ")
-local sudo=io.read()
-local text,ok = io.open("base.lua",'r'):read('*a'):gsub("FTABCHI%-ID",last)
-io.open("ftabchi-"..last..".lua",'w'):write(text):close()
-io.open("ftabchi-"..last..".sh",'w'):write("while true; do\n$(dirname $0)/telegram-cli-1222 -p ftabchi-"..last.." -s ftabchi-"..last..".lua\ndone"):close()
-io.popen("chmod 777 tabchi-"..last..".sh")
-redis:set('ftabchi:'..last..':fullsudo',sudo)
-print("Done!\nNew Ftabchi Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./ftabchi-"..last..".sh\nDeveloped by: ernestteam")
+
+io.write("Enter Tabchi ID : ")
+
+local last = io.read()
+
+io.popen('rm -rf ~/.telegram-cli/tabchi-'..last..' tabchi-'..last..'.lua tabchi-'..last..'.sh tabchi_'..last..'_logs.txt')
+
+redis:del('tabchi:'..last..':*')
+
+print("Done!\nAll Data/Files Of Tabchi Deleted\nTabchi ID : "..last)
+
